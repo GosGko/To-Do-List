3645e63d80ae7789c9306f3c8d26a1472879f528
GosGko <122621882+GosGko@users.noreply.github.com>
2023-10-15 14:15:15 +0300

    smthing

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 let date = new Date().toLocaleDateString('en-us', { weekday:"long", month:"short", day:"numeric"} );
 
-
 app.get("/", (req, res) => {
     res.render("index.ejs", {
         date: date,
@@ -18,7 +17,15 @@ app.get("/", (req, res) => {
 });
 
 app.post("/", (req, res) => {
-    console.log("good")
+    const thing = (req.body["newItem"]);
+    res.render("index.ejs", {
+        date: date,
+        thing: thing, 
+    });
+});
+
+app.post("/work", (req, res) => {
+    res.render("index.ejs");
 });
 
 app.listen(port, () => {
