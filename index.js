40c68be3c2080ac0c4de0c97ef2b02dfebfcf4ef
GosGko <122621882+GosGko@users.noreply.github.com>
2023-10-29 19:13:18 +0200

    done

--- index.js
+++ index.js
@@ -8,38 +8,39 @@ app.use(express.static("public"))
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+var toDoListDay = [];
+var toDoListWork = [];
+
 let date = new Date().toLocaleDateString('en-us', { weekday:"long", month:"short", day:"numeric"} );
 let year = new Date().getFullYear()
+
+app.post("/", (req, res) => {
+    const thing = req.body.newItem;
+    toDoListDay.push(thing);
+    res.redirect("/")
+});
 
 app.get("/", (req, res) => {
     res.render("index.ejs", {
         date: date,
         year: year,
+        toDoListDay: toDoListDay,
     });
 });
 
-app.post("/", (req, res) => {
-    const thing = (req.body["newItem"]);
-    res.render("index.ejs", {
-        date: date,
-        thing: thing, 
-        year: year,
-    });
+app.post("/work", (req, res) => {
+    const thing = req.body.newItem;
+    toDoListWork.push(thing);
+    res.redirect("/work")
 });
 
 app.get("/work", (req, res) => {
     res.render("index.ejs", {
         year: year,
+        toDoListWork: toDoListWork,
     });
 });
 
-app.post("/work", (req, res) => {
-    const thing = (req.body["newItem"]);
-    res.render("index.ejs", {
-        thing: thing, 
-        year: year,
-    });
-});
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
